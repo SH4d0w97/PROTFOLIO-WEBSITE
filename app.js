25d03c687c2575c1f41b6d9ee010d97ba63960a7
SH4d0w97 <soorajsr97@gmail.com>
2023-03-18 13:25:23 +0530

    implemented page transition function in js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const sections = document.querySelectorAll('.section');
 const sectionBtns = document.querySelectorAll('.controlls');
 const sectionBtn = document.querySelectorAll('.control');
+
+function PageTransitions(){
+    // Button click active class
+    for (let i = 0; i < sectionBtn.length; i++){
+        sectionBtn[i].addEventListener('click',function() {
+            let currentbtn = document.querySelectorAll('.active-btn');
+            currentbtn[0].className = currentbtn[0].className.replace('active-btn','');
+            this.className += 'active-btn';
+        })
+    }
+}
+
+PageTransitions();
